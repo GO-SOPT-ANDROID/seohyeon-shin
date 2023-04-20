a288819100e54489e53b75b88ad21294b1e2867f
ss99x2002 <ss99x200211@gmail.com>
2023-04-21 01:41:54 +0900

    [Feat] MainActivity Bottom nav 추가

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -2,13 +2,36 @@ package org.android.go.sopt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import org.android.go.sopt.databinding.ActivityMainBinding
+import org.android.go.sopt.fragment.GalleryFragment
+import org.android.go.sopt.fragment.HomeFragment
+import org.android.go.sopt.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNav()
+    }
 
+    private fun setBottomNav(){
+        binding.bnvMain.setOnItemSelectedListener {
+            changeFragment(
+                when(it.itemId){
+                    R.id.menu_home -> HomeFragment()
+                    R.id.menu_gallery->GalleryFragment()
+                    else -> SearchFragment()
+                }
+            )
+            true
+        }
+    }
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main,fragment)
+            .commit()
     }
 }
