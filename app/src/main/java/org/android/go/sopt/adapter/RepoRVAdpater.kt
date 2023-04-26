bae28a9ca6103b081d39f2d542020b5159e95201
ss99x2002 <ss99x200211@gmail.com>
2023-04-26 21:02:38 +0900

    [Feat] Repository id data 추가

--- app/src/main/java/org/android/go/sopt/adapter/RepoRVAdpater.kt
+++ app/src/main/java/org/android/go/sopt/adapter/RepoRVAdpater.kt
@@ -3,6 +3,7 @@ package org.android.go.sopt.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,17 +11,16 @@ import org.android.go.sopt.data.Repository
 import org.android.go.sopt.databinding.ItemRepoBinding
 
 class RepoRVAdapter(context: Context) : ListAdapter<Repository, RepoRVAdapter.RepoViewHolder>(diffUtil) {
-
     private val inflater by lazy { LayoutInflater.from(context) }
 
     private val repoList:List<Repository> = listOf(
-        Repository("Repository1","name1"),
-        Repository("Repository2","name2"),
-        Repository("Repository3","name3"),
-        Repository("Repository4","name4"),
-        Repository("Repository5","name5"),
-        Repository("Repository6","name6"),
-        Repository("Repository7","name7")
+        Repository(1,"Repository1","name1"),
+        Repository(2,"Repository2","name2"),
+        Repository(3,"Repository3","name3"),
+        Repository(4,"Repository4","name4"),
+        Repository(5,"Repository5","name5"),
+        Repository(6,"Repository6","name6"),
+        Repository(7,"Repository7","name7")
     )
 
     override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
@@ -38,8 +38,7 @@ class RepoRVAdapter(context: Context) : ListAdapter<Repository, RepoRVAdapter.Re
     companion object{
         val diffUtil = object:DiffUtil.ItemCallback<Repository>(){
             override fun areItemsTheSame(oldItem: Repository, newItem: Repository): Boolean {
-                return oldItem.name == newItem.name
-            // item의 고유한 정보로 비교하면되는데 Repo data에서 title보다는 name이 적절할 거 같아 name으로 비교.
+                return oldItem.id == newItem.id
             }
             override fun areContentsTheSame(oldItem: Repository, newItem: Repository): Boolean {
                 return oldItem == newItem
